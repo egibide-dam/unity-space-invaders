4ad6023ca734a8bf023dd9fc7aa857d8665d96c6
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2016-04-30 20:04:24 +0200

    Marcador con recuento de puntuación

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class ControlAlien : MonoBehaviour
 {
+	// Conexión al marcador, para poder actualizarlo
+	public GameObject marcador;
+
+	// Por defecto, 100 puntos por cada alien
+	public int puntos = 100;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		// Localizamos el objeto que contiene el marcador
+		marcador = GameObject.Find ("Marcador");
 	}
 	
 	// Update is called once per frame
@@ -23,8 +29,11 @@ public class ControlAlien : MonoBehaviour
 		// Necesitamos saber contra qué hemos chocado
 		if (coll.gameObject.tag == "disparo") {
 
+			// Sumar la puntuación al marcador
+			marcador.GetComponent<ControlMarcador> ().puntos += puntos;
+
 			// El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
-			Destroy(coll.gameObject);
+			Destroy (coll.gameObject);
 
 			// El alien desaparece
 			Destroy (gameObject);
