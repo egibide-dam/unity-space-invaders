31e0e0fad235fab199f2094f4c28ad56205c19f9
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-03-12 17:17:11 +0100

    Actualizado a la versión 2021.2.14f1

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlNave.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlNave.cs
@@ -4,72 +4,81 @@ using System.Collections;
 public class ControlNave : MonoBehaviour
 {
 
-	// Velocidad a la que se desplaza la nave (medido en u/s)
-	private float velocidad = 20f;
+    // Velocidad a la que se desplaza la nave (medido en u/s)
+    private float velocidad = 20f;
 
-	// Fuerza de lanzamiento del disparo
-	private float fuerza = 0.5f;
+    // Fuerza de lanzamiento del disparo
+    private float fuerza = 0.5f;
 
-	// Acceso al prefab del disparo
-	public Rigidbody2D disparo;
+    // Acceso al prefab del disparo
+    public Rigidbody2D disparo;
 
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		// Calculamos la anchura visible de la cámara en pantalla
-		float distanciaHorizontal = Camera.main.orthographicSize * Screen.width / Screen.height;
+    // Use this for initialization
+    void Start()
+    {
 
-		// Calculamos el límite izquierdo y el derecho de la pantalla
-		float limiteIzq = -1.0f * distanciaHorizontal;
-		float limiteDer = 1.0f * distanciaHorizontal;
+    }
 
-		// Tecla: Izquierda
-		if (Input.GetKey (KeyCode.LeftArrow)) {
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculamos la anchura visible de la cámara en pantalla
+        float distanciaHorizontal = Camera.main.orthographicSize * Screen.width / Screen.height;
 
-			// Nos movemos a la izquierda hasta llegar al límite para entrar por el otro lado
-			if (transform.position.x > limiteIzq) {
-				transform.Translate (Vector2.left * velocidad * Time.deltaTime);
-			} else {
-				transform.position = new Vector2 (limiteDer, transform.position.y);			
-			}
-		}
+        // Calculamos el límite izquierdo y el derecho de la pantalla
+        float limiteIzq = -1.0f * distanciaHorizontal;
+        float limiteDer = 1.0f * distanciaHorizontal;
 
-		// Tecla: Derecha
-		if (Input.GetKey (KeyCode.RightArrow)) {
+        // Tecla: Izquierda
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
 
-			// Nos movemos a la derecha hasta llegar al límite para entrar por el otro lado
-			if (transform.position.x < limiteDer) {
-				transform.Translate (Vector2.right * velocidad * Time.deltaTime);
-			} else {
-				transform.position = new Vector2 (limiteIzq, transform.position.y);			
-			}
-		}
+            // Nos movemos a la izquierda hasta llegar al límite para entrar por el otro lado
+            if (transform.position.x > limiteIzq)
+            {
+                transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = new Vector2(limiteDer, transform.position.y);
+            }
+        }
 
-		// Disparo
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			disparar ();
-		}
-	}
+        // Tecla: Derecha
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
 
-	void disparar ()
-	{
-		// Hacemos copias del prefab del disparo y las lanzamos
-		Rigidbody2D d = (Rigidbody2D)Instantiate (disparo, transform.position, transform.rotation);
+            // Nos movemos a la derecha hasta llegar al límite para entrar por el otro lado
+            if (transform.position.x < limiteDer)
+            {
+                transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = new Vector2(limiteIzq, transform.position.y);
+            }
+        }
 
-		// Desactivar la gravedad para este objeto, si no, ¡se cae!
-		d.gravityScale = 0;
+        // Disparo
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            disparar();
+        }
+    }
 
-		// Posición de partida, en la punta de la nave
-		d.transform.Translate (Vector2.up * 0.7f);
+    void disparar()
+    {
+        // Hacemos copias del prefab del disparo y las lanzamos
+        Rigidbody2D d = (Rigidbody2D)Instantiate(disparo, transform.position, transform.rotation);
 
-		// Lanzarlo
-		d.AddForce (Vector2.up * fuerza, ForceMode2D.Impulse);	
-	}
+        // Desactivar la gravedad para este objeto, si no, ¡se cae!
+        d.gravityScale = 0;
+
+        // Posición de partida, en la punta de la nave
+        d.transform.Translate(Vector2.up * 0.7f);
+
+        // Lanzarlo
+        d.AddForce(Vector2.up * fuerza, ForceMode2D.Impulse);
+    }
 
 }
