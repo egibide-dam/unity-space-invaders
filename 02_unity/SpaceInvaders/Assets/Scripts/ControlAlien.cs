4bf256e530c665fd52583003ff87fcd1bfd33936
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-03-12 17:16:04 +0100

    Actualizado a la versión 2021.2.14f1

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
@@ -4,30 +4,31 @@ using System.Collections;
 public class ControlAlien : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	void OnCollisionEnter2D (Collision2D coll)
-	{
-		// Detectar la colisión entre el alien y otros elementos
+    }
 
-		// Necesitamos saber contra qué hemos chocado
-		if (coll.gameObject.tag == "disparo") {
+    // Update is called once per frame
+    void Update()
+    {
 
-			// El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
-			Destroy(coll.gameObject);
+    }
 
-			// El alien desaparece
-			Destroy (gameObject);
-		}
-	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // Detectar la colisión entre el alien y otros elementos
+
+        // Necesitamos saber contra qué hemos chocado
+        if (coll.gameObject.tag == "disparo")
+        {
+
+            // El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
+            Destroy(coll.gameObject);
+
+            // El alien desaparece
+            Destroy(gameObject);
+        }
+    }
 }
