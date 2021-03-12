7b90f617784df65854d4c99a09163b2be93cac86
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-03-12 17:15:29 +0100

    Actualizado a la versión 6000.3.10f1

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlCamara.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlCamara.cs
@@ -3,30 +3,32 @@ using System.Collections;
 
 public class ControlCamara : MonoBehaviour
 {
-	// Referencia al objeto en la escena
-	private GameObject alien;
+    // Referencia al objeto en la escena
+    private GameObject alien;
 
-	// Velocidad a la que se desplaza el alien
-	private float velocidad = 20f;
+    // Velocidad a la que se desplaza el alien
+    private float velocidad = 20f;
 
-	// Use this for initialization
-	void Start ()
-	{
-		// Conectamos con la instancia que hemos creado en el editor
-		alien = GameObject.Find ("Alien1");
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		// Tecla: Izquierda
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			alien.transform.Translate (Vector2.left * velocidad * Time.deltaTime);
-		}
+    // Use this for initialization
+    void Start()
+    {
+        // Conectamos con la instancia que hemos creado en el editor
+        alien = GameObject.Find("Alien1");
+    }
 
-		// Tecla: Derecha
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			alien.transform.Translate (Vector2.right * velocidad * Time.deltaTime);
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        // Tecla: Izquierda
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            alien.transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+        }
+
+        // Tecla: Derecha
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            alien.transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+        }
+    }
 }
--- 02_unity/SpaceInvaders/Assets/Scripts/GeneradorAliens.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/GeneradorAliens.cs
@@ -4,49 +4,51 @@ using System.Collections;
 public class GeneradorAliens : MonoBehaviour
 {
 
-	// Publicamos la variable para conectarla desde el editor
-	public Rigidbody2D prefabAlien1;
+    // Publicamos la variable para conectarla desde el editor
+    public Rigidbody2D prefabAlien1;
 
-	// Use this for initialization
-	void Start ()
-	{
-		// Rejilla de 4x7 aliens
-		generarAliens (4, 7, 1.5f, 1.0f);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // Rejilla de 4x7 aliens
+        generarAliens(4, 7, 1.5f, 1.0f);
+    }
 
-	void generarAliens (int filas, int columnas, float espacioH, float espacioV, float escala = 1.0f)
-	{
-		/* Creamos una rejilla de aliens a partir del punto de origen
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void generarAliens(int filas, int columnas, float espacioH, float espacioV, float escala = 1.0f)
+    {
+        /* Creamos una rejilla de aliens a partir del punto de origen
 		 * 
 		 * Ejemplo (2,5):
 		 *   A A A A A
 		 *   A A O A A
 		 */
 
-		// Calculamos el punto de origen de la rejilla
-		Vector2 origen = new Vector2 (transform.position.x - (columnas / 2.0f) * espacioH + (espacioH / 2), transform.position.y);
+        // Calculamos el punto de origen de la rejilla
+        Vector2 origen = new Vector2(transform.position.x - (columnas / 2.0f) * espacioH + (espacioH / 2), transform.position.y);
 
-		// Generamos los objetos
-		for (int i = 0; i < filas; i++) {
-			for (int j = 0; j < columnas; j++) {
-				// Posición de cada alien
-				Vector2 posicion = new Vector2 (origen.x + (espacioH * j), origen.y + (espacioV * i));
+        // Generamos los objetos
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                // Posición de cada alien
+                Vector2 posicion = new Vector2(origen.x + (espacioH * j), origen.y + (espacioV * i));
 
-				// Instanciamos el objeto partiendo del prefab
-				Rigidbody2D alien = (Rigidbody2D)Instantiate (prefabAlien1, posicion, transform.rotation);
+                // Instanciamos el objeto partiendo del prefab
+                Rigidbody2D alien = (Rigidbody2D)Instantiate(prefabAlien1, posicion, transform.rotation);
 
-				// Escala opcional, por defecto 1.0f (sin escala)
-				// Nota: El prefab original ya está escalado a 0.2f
-				alien.transform.localScale = new Vector2 (0.2f * escala, 0.2f * escala);
-			}
-		}
+                // Escala opcional, por defecto 1.0f (sin escala)
+                // Nota: El prefab original ya está escalado a 0.2f
+                alien.transform.localScale = new Vector2(0.2f * escala, 0.2f * escala);
+            }
+        }
 
-	}
+    }
 
 }
