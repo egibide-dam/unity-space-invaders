96f5269111c4364e72dd992e6645d50dad0bc9cc
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-03-12 17:16:23 +0100

    Actualizado a la versión 2021.2.14f1

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
@@ -3,40 +3,41 @@ using System.Collections;
 
 public class ControlAlien : MonoBehaviour
 {
-	// Conexión al marcador, para poder actualizarlo
-	public GameObject marcador;
+    // Conexión al marcador, para poder actualizarlo
+    public GameObject marcador;
 
-	// Por defecto, 100 puntos por cada alien
-	public int puntos = 100;
+    // Por defecto, 100 puntos por cada alien
+    public int puntos = 100;
 
-	// Use this for initialization
-	void Start ()
-	{
-		// Localizamos el objeto que contiene el marcador
-		marcador = GameObject.Find ("Marcador");
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // Localizamos el objeto que contiene el marcador
+        marcador = GameObject.Find("Marcador");
+    }
 
-	void OnCollisionEnter2D (Collision2D coll)
-	{
-		// Detectar la colisión entre el alien y otros elementos
+    // Update is called once per frame
+    void Update()
+    {
 
-		// Necesitamos saber contra qué hemos chocado
-		if (coll.gameObject.tag == "disparo") {
+    }
 
-			// Sumar la puntuación al marcador
-			marcador.GetComponent<ControlMarcador> ().puntos += puntos;
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // Detectar la colisión entre el alien y otros elementos
 
-			// El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
-			Destroy (coll.gameObject);
+        // Necesitamos saber contra qué hemos chocado
+        if (coll.gameObject.tag == "disparo")
+        {
 
-			// El alien desaparece
-			Destroy (gameObject);
-		}
-	}
+            // Sumar la puntuación al marcador
+            marcador.GetComponent<ControlMarcador>().puntos += puntos;
+
+            // El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
+            Destroy(coll.gameObject);
+
+            // El alien desaparece
+            Destroy(gameObject);
+        }
+    }
 }
