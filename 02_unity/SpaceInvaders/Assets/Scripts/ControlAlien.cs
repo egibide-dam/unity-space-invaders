aea6c596d3176e5c89c120bd405eb42085f465a2
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-03-12 17:16:49 +0100

    Actualizado a la versión 2021.2.14f1

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
@@ -3,49 +3,50 @@ using System.Collections;
 
 public class ControlAlien : MonoBehaviour
 {
-	// Conexión al marcador, para poder actualizarlo
-	public GameObject marcador;
+    // Conexión al marcador, para poder actualizarlo
+    public GameObject marcador;
 
-	// Por defecto, 100 puntos por cada alien
-	public int puntos = 100;
+    // Por defecto, 100 puntos por cada alien
+    public int puntos = 100;
 
-	// Use this for initialization
-	void Start ()
-	{
-		// Localizamos el objeto que contiene el marcador
-		marcador = GameObject.Find ("Marcador");
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // Localizamos el objeto que contiene el marcador
+        marcador = GameObject.Find("Marcador");
+    }
 
-	void OnCollisionEnter2D (Collision2D coll)
-	{
-		// Detectar la colisión entre el alien y otros elementos
+    // Update is called once per frame
+    void Update()
+    {
 
-		// Necesitamos saber contra qué hemos chocado
-		if (coll.gameObject.tag == "disparo") {
+    }
 
-			// Sonido de explosión
-			GetComponent<AudioSource> ().Play ();
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // Detectar la colisión entre el alien y otros elementos
 
-			// Sumar la puntuación al marcador
-			marcador.GetComponent<ControlMarcador> ().puntos += puntos;
+        // Necesitamos saber contra qué hemos chocado
+        if (coll.gameObject.tag == "disparo")
+        {
 
-			// El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
-			Destroy (coll.gameObject);
+            // Sonido de explosión
+            GetComponent<AudioSource>().Play();
 
-			// El alien desaparece (hay que añadir un retraso, si no, no se oye la explosión)
+            // Sumar la puntuación al marcador
+            marcador.GetComponent<ControlMarcador>().puntos += puntos;
 
-			// Lo ocultamos...
-			GetComponent<Renderer> ().enabled = false;
-			GetComponent<Collider2D> ().enabled = false;
+            // El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
+            Destroy(coll.gameObject);
 
-			// ... y lo destruímos al cabo de 5 segundos, para dar tiempo al efecto de sonido
-			Destroy (gameObject, 5f);
-		}
-	}
+            // El alien desaparece (hay que añadir un retraso, si no, no se oye la explosión)
+
+            // Lo ocultamos...
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+
+            // ... y lo destruímos al cabo de 5 segundos, para dar tiempo al efecto de sonido
+            Destroy(gameObject, 5f);
+        }
+    }
 }
